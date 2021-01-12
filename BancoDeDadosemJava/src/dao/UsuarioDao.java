2ce95afb28eb76af9af73b66a06f49cb7ecba803
Marcos Paulo Carneiro Tavares <46692865+marcoscarneiro2@users.noreply.github.com>
2021-01-12 12:04:00 -0300

    Add files via upload

---
+++ BancoDeDadosemJava/src/dao/UsuarioDao.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Usuario;
+import view.FormCadastroView;
+
+/**
+ *
+ * @author usuario
+ */
+public class UsuarioDao {
+    private final Connection connection;
+    
+    public UsuarioDao(Connection connection){
+        this.connection = connection;
+    }
+    public void insert(Usuario usuario) throws SQLException{
+            String sql = "insert into usuario(usuario, senha) values ('"+usuario.getUsuario() +"','"+usuario.getSenha()+"')";
+        
+            PreparedStatement statement = connection.prepareStatement(sql);
+            
+            statement.execute();
+           
+            connection.close();
+            
+       
+    }
+}
